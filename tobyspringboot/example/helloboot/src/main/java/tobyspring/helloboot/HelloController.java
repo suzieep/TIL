37d148023eb7bad06cdcad410c62f4b473517a10
suzieep <wnsuwls0707@gmail.com>
2023-04-14 10:03:51 +0900

    토비 - helloboot example

--- tobyspringboot/example/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ tobyspringboot/example/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,19 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    private final HelloService helloService; //final은 재할당이 불가능하기 때문에 처음 정의했을 때 초기화를 해주거나 적어도 생성자에서 초기화 해줘야 한다
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
     public String hello(String name){
-        return "Hello "+ name;
+        if(name == null|| name.trim().length()==0) throw new IllegalArgumentException();
+        return helloService.sayHello(name);
     }
 }
+
--- tobyspringboot/example/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ tobyspringboot/example/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,17 +1,29 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration //구성정보를 가지고 있는 class다 이 안에 bean annotation이 붙은 factory method가 있겠구나 생각하고 조회하게 됨
+@ComponentScan //이 클래스가 있는 패키지부터 하위 패키지를 뒤져서 component가 붙어있는 모든 클래스 찾아 -> 매번 구성정보 저장할 필요는 없음(편해서 표준으로 사용중), 하지만 어떤 클래스가 붙는지 바로 위계구조를 확인하기가 어려운 단점이 있음
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); //도우미 클래스
-		WebServer webServer = serverFactory.getWebServer();
-		webServer.start(); //TomcatServlet Container run
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory(){
+		return new TomcatServletWebServerFactory();
 	}
+	@Bean
+	public DispatcherServlet dispatcherServlet(){
+		return new DispatcherServlet();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HellobootApplication.class,args);
+
+	}
+
 
 }
