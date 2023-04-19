47a4fd959137797f9ce62a11f65fcc98196ff087
suzieep <wnsuwls0707@gmail.com>
2023-04-19 15:16:31 +0900

    스프링DB section1

--- tobyspringboot/example/helloboot/src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
+++ tobyspringboot/example/helloboot/src/main/java/tobyspring/config/autoconfig/DispatcherServletConfig.java
@@ -3,7 +3,6 @@ package tobyspring.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
